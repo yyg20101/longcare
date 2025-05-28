ae87a8c3795f979e75bb5fa765f6e03727396ea6
yuyingui <250770503@qq.com>
2025-05-28 14:34:42 +0800

    feat 添加 benchmark

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlinCompose) apply false
     alias(libs.plugins.room) apply false
+    alias(libs.plugins.benchmark) apply false
     alias(libs.plugins.wire) apply false
     alias(libs.plugins.ktlint) apply false
 }
