36bd775c7e8d901736be532749d86a4b87418fc1
wajie <wajie20101@gmail.com>
2026-02-11 20:55:31 +0800

    refactor(login): hide test-entry buttons in release builds

--- app/src/main/kotlin/com/ytone/longcare/debug/NfcTestConfig.kt
+++ app/src/main/kotlin/com/ytone/longcare/debug/NfcTestConfig.kt
@@ -1,5 +1,7 @@
 package com.ytone.longcare.debug
 
+import com.ytone.longcare.BuildConfig
+
 /**
  * NFC测试功能配置
  * 控制NFC测试功能的开关，便于后期删除
@@ -9,7 +11,7 @@ object NfcTestConfig {
      * 是否启用NFC测试功能
      * 发布时设为false，或直接删除相关代码
      */
-    const val ENABLE_NFC_TEST = true
+    val ENABLE_NFC_TEST = BuildConfig.DEBUG
     
     /**
      * 测试功能标识
